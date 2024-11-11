c6638fe985f51f6616fa8899fbf35c0c4d3040ab
Hazzoz <hwils2019@gmail.com>
2024-11-12 10:35:00 +1100

    Update function descriptions and added log scale to coverage error

--- src/mycode.jl
+++ src/mycode.jl
@@ -72,21 +72,14 @@ function SIR!(dP, P, params::SIRFoIIntervention, t)
 end
 
 """
-# solve_SIR is a driver function that chooses the required SIR model.
-# Inputs:
-# - S0 = Initial Susceptible Population
-# - I0 = Initial Infected Population
-# - SI0 = Initial Seriously Infected Population
-# - R0 = Initial Recovered Population
-# - days = No. of days modelled 
-# - params = array of other necessary parameters
-#   - beta = transmission chance of any interaction
-#   - gamma = recovery rate
-#   - contacts = number of daily contacts a person has
-#   - SIratio = proportion of people being seriously infected
-#   - delta = recovery rate of seriously infected
-#   - alpha = number of recovered people losing immunity
-#   - lambdas = recorded array of lambdas with timestamps
+solve_SIR is a driver function that chooses the required SIR model.
+Inputs:
+- S0 = Initial Susceptible Population
+- I0 = Initial Infected Population
+- SI0 = Initial Seriously Infected Population
+- R0 = Initial Recovered Population
+- days = No. of days modelled 
+- params = array of other necessary parameters, defined by the structs
 """
 function solve_SIR(S0, I0, SI0, R0, days, params)
     P0 = [S0, I0, SI0, R0] # Initial populations vector
@@ -99,20 +92,14 @@ function solve_SIR(S0, I0, SI0, R0, days, params)
 end
 
 """
-# plot_SIR is a driver function that runs and then plots and SIR model.
-# Inputs:
-# - S0 = Initial Susceptible Population
-# - I0 = Initial Infected Population
-# - SI0 = Initial Seriously Infected Population
-# - R0 = Initial Recovered Population
-# - days = No. of days modelled 
-# - params = array of other necessary parameters
-#   - beta = transmission chance of any interaction
-#   - gamma = recovery rate
-#   - contacts = number of daily contacts a person has
-#   - SIratio = proportion of people being seriously infected
-#   - delta = recovery rate of seriously infected
-#   - alpha = number of recovered people losing immunity
+plot_SIR is a driver function that runs and then plots and SIR model.
+Inputs:
+- S0 = Initial Susceptible Population
+- I0 = Initial Infected Population
+- SI0 = Initial Seriously Infected Population
+- R0 = Initial Recovered Population
+- days = No. of days modelled 
+- params = array of other necessary parameters, defined by the structs
 """
 function plot_SIR(S0, I0, SI0, R0, days, params)
     solution = solve_SIR(S0, I0, SI0, R0, days, params) # Solve the SIR model
@@ -121,23 +108,17 @@ function plot_SIR(S0, I0, SI0, R0, days, params)
 end
 
 """
-# plot_infected is a function that plots either the infected or
-# seriously infected data against the true data to allow for visual
-# inspection of the models fit.
-# Inputs:
-# - S0 = Initial Susceptible Population
-# - I0 = Initial Infected Population
-# - SI0 = Initial Seriously Infected Population
-# - R0 = Initial Recovered Population
-# - days = No. of days modelled 
-# - params = array of other necessary parameters
-#   - beta = transmission chance of any interaction
-#   - gamma = recovery rate
-#   - contacts = number of daily contacts a person has
-#   - SIratio = proportion of people being seriously infected
-#   - delta = recovery rate of seriously infected
-#   - alpha = number of recovered people losing immunity
-# - infect = determines whether to plot infected or seriously infected data
+plot_infected is a function that plots either the infected or
+seriously infected data against the true data to allow for visual
+inspection of the models fit.
+Inputs:
+- S0 = Initial Susceptible Population
+- I0 = Initial Infected Population
+- SI0 = Initial Seriously Infected Population
+- R0 = Initial Recovered Population
+- days = No. of days modelled 
+- params = array of other necessary parameters, defined by the structs
+- infect = determines whether to plot infected or seriously infected data (0 = infected, else = seriously infected)
 """
 function plot_infected(S0, I0, SI0, R0, days, params, infect, ratio_range)
     # Actual data up to day 30
@@ -202,22 +183,17 @@ function plot_infected(S0, I0, SI0, R0, days, params, infect, ratio_range)
 end
 
 """
-# error_beta is a function that plots the SME of a range of beta values
-# using the predicted and actual data provided. It is placed around the estimate
-# from level 2.
-# Inputs:
-# - S0 = Initial Susceptible Population
-# - I0 = Initial Infected Population
-# - SI0 = Initial Seriously Infected Population
-# - R0 = Initial Recovered Population
-# - days = No. of days modelled 
-# - params = array of other necessary parameters
-#   - beta = transmission chance of any interaction
-#   - gamma = recovery rate
-#   - contacts = number of daily contacts a person has
-#   - SIratio = proportion of people being seriously infected
-#   - delta = recovery rate of seriously infected
-#   - alpha = number of recovered people losing immunity
+error_beta is a function that plots the RSME of a range of beta values
+using the predicted and actual data provided.
+Inputs:
+- S0 = Initial Susceptible Population
+- I0 = Initial Infected Population
+- SI0 = Initial Seriously Infected Population
+- R0 = Initial Recovered Population
+- days = No. of days modelled 
+- params = array of other necessary parameters, defined by the structs
+- beta_range = range of betas that are to be tested
+- ratio_range = range of proportions of people that become seriously infected
 """
 function error_beta(S0, I0, SI0, R0, days, params, beta_range, ratio_range)
     # Actual data up to day 30
@@ -286,6 +262,21 @@ function error_beta(S0, I0, SI0, R0, days, params, beta_range, ratio_range)
     plot!(betas, error_vals, xlabel="Beta", ylabel="Error", title="Beta vs Error", labels=nothing, colour=:blue)
 end
 
+"""
+plot_intervention_with_error is a function that plots the behaviour of the infected and 
+seriously infected curves from the model with the effect of the intervention, with 
+associated uncertainties.
+Inputs:
+- S0 = Initial Susceptible Population
+- I0 = Initial Infected Population
+- SI0 = Initial Seriously Infected Population
+- R0 = Initial Recovered Population
+- days = No. of days modelled 
+- params = array of other necessary parameters for the model before intervention, defined by the structs
+- beta_range = range of betas that are to be tested
+- params2 = array of other necessary parametersfor the model after intervention, defined by the structs
+- ratio_range = range of proportions of people that become seriously infected
+"""
 function plot_intervention_with_error(S0, I0, SI0, R0, days, params, beta_range, params2, ratio_range)
     max_infected_peak = 0
     min_infected_peak = 0
@@ -374,8 +365,24 @@ function plot_intervention_with_error(S0, I0, SI0, R0, days, params, beta_range,
     plot!(time, seriously_infected, xlabel="Time (Days)", ylabel="Population", title="Infected After Intervention", labels="Seriously Infected", color=:red)
 end
 
-
-function compare_intervention(S0, I0, SI0, R0, days, params, beta_range, params2, infect, ratio_range, intervene)
+"""
+compare_intervention is a function that plots the behaviour of the infected or 
+seriously infected curves from the model with or without the effect of the intervention,
+with associated uncertainties.
+Inputs:
+- S0 = Initial Susceptible Population
+- I0 = Initial Infected Population
+- SI0 = Initial Seriously Infected Population
+- R0 = Initial Recovered Population
+- days = No. of days modelled 
+- params = array of other necessary parameters for the model before intervention, defined by the structs
+- beta_range = range of betas that are to be tested
+- params2 = array of other necessary parametersfor the model after intervention, defined by the structs
+- ratio_range = range of proportions of people that become seriously infected
+- infect = determines whether to plot infected or seriously infected data (0 = infected, else = seriously infected)
+- intervent = determines whether to plot the intervention or no intervention curve (0 = intervention, else = no intervention)
+"""
+function compare_intervention(S0, I0, SI0, R0, days, params, beta_range, params2, ratio_range, infect, intervene)
     max_infected_peak = 0
     min_infected_peak = 0
     max_seriously_infected_peak = 0
@@ -422,7 +429,7 @@ function compare_intervention(S0, I0, SI0, R0, days, params, beta_range, params2
                     push!(previous_seriously_infected,previous_seriously_infected[end])
                 end
 
-                if infect == 0
+                if infect != 0
                     plot!(solution.t, infected, xlabel="Time (Days)", ylabel="Population", title="Infected No Intervention", labels=nothing, color=:red, fillrange=previous_infected)
                 else
                     plot!(solution.t, seriously_infected, xlabel="Time (Days)", ylabel="Population", title="Seriously Infected No Intervention", labels=nothing, color=:red, fillrange=previous_seriously_infected)
@@ -490,7 +497,7 @@ function compare_intervention(S0, I0, SI0, R0, days, params, beta_range, params2
                     push!(previous_seriously_infected,previous_seriously_infected[end])
                 end
 
-                if infect == 0
+                if infect != 0
                     plot!(time, infected, xlabel="Time (Days)", ylabel="Population", title="Infected Intervention", labels=nothing, color=:blue, fillrange=previous_infected, alpha=0.1)
                 else
                     plot!(time, seriously_infected, xlabel="Time (Days)", ylabel="Population", title="Seriously Infected Intervention", labels=nothing, color=:blue, fillrange=previous_seriously_infected, alpha=0.1)
@@ -516,13 +523,13 @@ function compare_intervention(S0, I0, SI0, R0, days, params, beta_range, params2
     end
 
     if infect == 0
-        if intervene == 0
+        if intervene != 0
             println("Peak Infected No Intervention: ", round(min_infected_peak,digits=0), " - ", round(max_infected_peak,digits=0))
         else
             println("Peak Infected Intervention: ", round(min_infected_peak_intervention,digits=0), " - ", round(max_infected_peak_intervention,digits=0))
         end
     else
-        if intervene == 0
+        if intervene != 0
             println("Peak Seriously Infected No Intervention: ", round(min_seriously_infected_peak,digits=0), " - ", round(max_seriously_infected_peak,digits=0))
         else
             println("Peak Seriously Infected Intervention: ", round(min_seriously_infected_peak_intervention,digits=0), " - ", round(max_seriously_infected_peak_intervention,digits=0))
@@ -531,24 +538,23 @@ function compare_intervention(S0, I0, SI0, R0, days, params, beta_range, params2
 end
 
 """
-# error_beta is a function that plots the SME of a range of beta values
-# using the predicted and actual data provided. It is placed around the estimate
-# from level 2.
-# Inputs:
-# - S0 = Initial Susceptible Population
-# - I0 = Initial Infected Population
-# - SI0 = Initial Seriously Infected Population
-# - R0 = Initial Recovered Population
-# - days = No. of days modelled 
-# - params = array of other necessary parameters
-#   - beta = transmission chance of any interaction
-#   - gamma = recovery rate
-#   - contacts = number of daily contacts a person has
-#   - SIratio = proportion of people being seriously infected
-#   - delta = recovery rate of seriously infected
-#   - alpha = number of recovered people losing immunity
+error_coverage is a function that plots the RSME of a range of coverage values
+using the predicted and actual data provided. It is incorporates the uncertainty
+of beta and ratio, and can be plotted logarithmically to better highlight errors.
+Inputs:
+- S0 = Initial Susceptible Population
+- I0 = Initial Infected Population
+- SI0 = Initial Seriously Infected Population
+- R0 = Initial Recovered Population
+- days = No. of days modelled 
+- params = array of other necessary parameters for the model before intervention, defined by the structs
+- coverage_range = range of coverages that are to be tested
+- params2 = array of other necessary parametersfor the model after intervention, defined by the structs
+- beta_range = range of transmission rates of disease
+- ratio_range = range of proportions of people that become seriously infected
+- log = plots the error logarithmically (0 = log, else = not log)
 """
-function error_coverage(S0, I0, SI0, R0, days, params, coverage_range, params2, beta_range, ratio_range)
+function error_coverage(S0, I0, SI0, R0, days, params, coverage_range, params2, beta_range, ratio_range, log)
     # Actual data up to day 55
     actual_infected = [11,7,20,3,29,14,11,12,16,10,58,34,26,29,51,55,155,53,67,98,130,189,92,192,145,128,68,74,126,265,154,207,299,273,190,152,276,408,267,462,352]
     ti = [15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55]
@@ -615,7 +621,11 @@ function error_coverage(S0, I0, SI0, R0, days, params, coverage_range, params2, 
                         end
                     end
                 end
-                push!(error_vals, sqrt(current_infected_error + current_seriously_infected_error)) # Calculate RSME at coverage value
+                if log == 0
+                    push!(error_vals, log(sqrt(current_infected_error + current_seriously_infected_error))) # Calculate RSME at coverage value
+                else
+                    push!(error_vals, sqrt(current_infected_error + current_seriously_infected_error)) # Calculate RSME at coverage value
+                end
             end
 
             if minimum(error_vals) < min
@@ -641,8 +651,24 @@ function error_coverage(S0, I0, SI0, R0, days, params, coverage_range, params2, 
     plot!(coverages, error_vals, xlabel="Coverage", ylabel="Error", title="Coverage vs Error", labels=nothing)
 end
 
+"""
+plot_coverage is a function that plots the infected or seriously infected model curves 
+with new coverage against the actual data. It is incorporates the uncertainty of beta 
+and ratio.
+Inputs:
+- S0 = Initial Susceptible Population
+- I0 = Initial Infected Population
+- SI0 = Initial Seriously Infected Population
+- R0 = Initial Recovered Population
+- days = No. of days modelled 
+- params = array of other necessary parameters for the model before intervention, defined by the structs
+- infect = determines whether to plot infected or seriously infected data (0 = infected, else = seriously infected)
+- params2 = array of other necessary parametersfor the model after intervention, defined by the structs, with new coverage
+- ratio_range = range of proportions of people that become seriously infected
+- beta_range = range of transmission rates of disease
+"""
 function plot_coverage(S0, I0, SI0, R0, days, params, infect, params2, ratio_range, beta_range)
-    # Actual data up to day 30
+    # Actual data up to day 55
     actual_infected = [11,7,20,3,29,14,11,12,16,10,58,34,26,29,51,55,155,53,67,98,130,189,92,192,145,128,68,74,126,265,154,207,299,273,190,152,276,408,267,462,352]
     ti = [15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55]
     actual_seriously_infected = [0,0,1,2,5,5,5,2,9,4,22,0,15,48,38,57,9,18,20,0,41,15,35,36,27,38,24,40,34,57,18,29,63,66,119]
